dd5c74f68e8f965255a44cd054c7c529c8fccd88
Adil Jamal <jamal.adil7860@gmail.com>
2026-04-16 20:48:53 +0530

    typecasting

---
+++ src/Typecasting.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Typecasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//		float num = input.nextFloat();
+//		int num = input.nextInt();
+//		System.out.println(num);
+
+        //Typecasting
+
+		float num = (int)(67);
+		System.out.println(num);
+
+//        int number = 'A';
+//        System.out.println(number);
+    }
+}
