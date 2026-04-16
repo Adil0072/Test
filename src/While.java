755cbaedf4b5db0bcbf0e7a70461d0f9dc0b2a16
Adil Jamal <Jamal.adil7860@gmail.com>
2026-04-16 20:53:57 +0530

    loop limit

--- src/While.java
+++ src/While.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class While {
     public static void main(String[] args) {
         int num = 1;
-        while(num <= 100) {
+        while(num <= 10) {
             System.out.println(num);
             num++;
         }
