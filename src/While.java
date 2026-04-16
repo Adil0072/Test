9895818eb56a323b35bd67abbce5e3d4e347f3d6
Adil Jamal <jamal.adil7860@gmail.com>
2026-04-16 20:52:43 +0530

    while

---
+++ src/While.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class While {
+    public static void main(String[] args) {
+        int num = 1;
+        while(num <= 100) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
