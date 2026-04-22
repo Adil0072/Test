ad3fe578f10aebf8df680ef165f02ad22f265eee
Adil Jamal <jamal.adil7860@gmail.com>
2026-04-22 18:15:54 +0530

    Armstrong number

---
+++ src/ArmstrongNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+//        for (int i = 100; i < 1000; i++) {
+//           if (isArmstrong(i)) {
+//              System.out.print(i + " ");
+//           }
+        System.out.println(isArmstrong(n));
+    }
+    static boolean isArmstrong(int n) {
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            n = n / 10;
+            sum = sum + rem*rem*rem;
+        }
+
+        return sum == original;
+    }
+}
