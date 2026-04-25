c2a46863a7bed3ad71574b2fcfed2e00182a3387
Adil Jamal <jamal.adil7860@gmail.com>
2026-04-25 19:58:56 +0530

    counting repeating numbers

--- src/Count.java
+++ src/Count.java
@@ -1,13 +1,13 @@
 public class Count {
     public static void main(String[] args) {
-        int n = 547;
+        int n = 5475;
         int count = 0;
         while(n > 0) {
             int rem = n % 10;
-            count ++;
-//			if (rem == 5) {
-//				count ++;
-//			}
+//            count ++;
+			if (rem == 5) {
+				count ++;
+			}
             n = n / 10;
         }
         System.out.println(count);
