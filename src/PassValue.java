a7433f9908da824897000cb8e50ec731dc54d615
Adil Jamal <jamal.adil7860@gmail.com>
2026-04-26 13:48:50 +0530

    pass by value

---
+++ src/PassValue.java
@@ -0,0 +1,10 @@
+public class PassValue {
+    public static void main(String[] args) {
+        int ans = sum(10,10);
+        System.out.println(ans);
+    }
+    static int sum(int a,int b){
+        int sum = a+b;
+        return sum;
+    }
+}
